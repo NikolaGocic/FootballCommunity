f6ff270c59c48c8231a37e7c8e7d80265e7413bf
Nikola Gocic <ngocic97@gmail.com>
2020-01-09 17:54:48 +0100

    New commit

--- Documents/Primer2/DataLayer/AutoMapper.cs
+++ Documents/Primer2/DataLayer/AutoMapper.cs
@@ -16,7 +16,7 @@ namespace DataLayer
             CreateMap<Fixture, FixtureView>();
             CreateMap<League, LeagueView>();
             CreateMap<MyFixture, MyFixtureView>();
-            CreateMap<PredictionType, PredictionType>();
+            CreateMap<PredictionType, PredictionTypeView>();
             CreateMap<Prediction, PredictionView>();
             CreateMap<Team, TeamView>();
             CreateMap<User, UserView>();
